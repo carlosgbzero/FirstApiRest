8133f264cf47ba576cc303ed05f1a9716cb8dde1
Sheila <carlosgbzero@gmail.com>
2025-01-16 10:08:30 -0500

    Actualizacion

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,14 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors(
+    {
+      origin: 'http://localhost:5173',
+      methods: 'GET,POST,PATCH,DELETE',
+      credentials:true,
+      allowedHeaders:'Content-Type,Authorization'
+    }
+  );
 
   app.useGlobalPipes(new ValidationPipe({
     whitelist : true,
